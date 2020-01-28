e61608f700f9033de2ecf474be1dd576c46b6396
crijacoran <cristian.cogollo@koombea.com>
2020-01-28 10:36:25 -0500

    test

--- test.tf
+++ test.tf
@@ -1,2 +1,2 @@
-ecs_service_route53_domain = "${var.url_ecs_service_route53_domains}"
-variable "url_ecs_service_route53_domain"    { default = "confirmapp.farzoo.clicks" }
+ecs_service_route53_domain = "${variable.url_ecs_service_route53_domain}"
+variable "url_ecs_service_route53_domain"    { default = "confirmapp.farzoo.click" }
